b9dcfcf13a688c7b88ac45f05d35a0a601c8ddae
rajatplease <itsrajatkaushik@gmail.com>
2021-02-25 00:19:38 +0530

    Changed theme, fixed scroll bug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import Editor from './Editor';
+import React, { useState, useEffect } from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+  const [html, setHtml] = useLocalStorage('html', '');
+  const [css, setCss] = useLocalStorage('css', '');
+  const [js, setJs] = useLocalStorage('js', '');
+  const [srcDoc, setSrcDoc] = useLocalStorage('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+  <html>
+  <body>${html}</body>
+  <style>${css}</style>
+  <script>${js}</script>
+  </html>
+  `);
+    }, 400);
+
+    return () => clearTimeout(timeout);
+  }, [js, html, css]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="js"
+          displayName="JavaScript"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameborder="0"
+          width="100%"
+          height="100%"
+        ></iframe>
+      </div>
+      <a href="https://github.com/itsRajat">
+        <h1>CODEZEN</h1>
+        <h2>DEVELOPED BY RAJAT KAUSHIK</h2>
+      </a>
+    </>
   );
 }
 
